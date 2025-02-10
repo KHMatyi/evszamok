b4d99baba28c148a05cc8cffed15a3ff90c5d60e
Mátyás Kovács-Haász <khmatyi@gmail.com>
2025-02-10 15:23:08 +0100

    validáció

--- main.js
+++ main.js
@@ -7,37 +7,37 @@
 const header = ["Időszak","Évszám","Esemény","Tananyag"]
 const starterData = [
     {
-        Időszak:"XVI. század",
-        Évszám:"1514",
-        Esemény:"Dózsa-féle parasztháború",
-        Tananyag:"magyar",
-        Évszám2:"1519-1522",
-        Esemény2:"Magellán körülhajózza a földet",
-        Tananyag2:"egyetemes",
+        korszak:"XVI. század",
+        evszam1:"1514",
+        megnev1:"Dózsa-féle parasztháború",
+        tan1:"magyar",
+        evszam2:"1519-1522",
+        megnev2:"Magellán körülhajózza a földet",
+        tan2:"egyetemes",
     },
     {
-        Időszak:"XVII. század",
-        Évszám:"1664",
-        Esemény:"vasvári béke",
-        Tananyag:"magyar",
+        korszak:"XVII. század",
+        evszam1:"1664",
+        megnev1:"vasvári béke",
+        tan1:"magyar",
     },
     {
-        Időszak:"XVIII. század",
-        Évszám:"1701-1714",
-        Esemény:"spanyol örökösödési háború",
-        Tananyag:"egyetemes",
-        Évszám2:"1703-1711",
-        Esemény2:"Rákóczi szabadságharc",
-        Tananyag2:"magyar",
+        korszak:"XVIII. század",
+        evszam1:"1701-1714",
+        megnev1:"spanyol örökösödési háború",
+        tan1:"egyetemes",
+        evszam2:"1703-1711",
+        megnev2:"Rákóczi szabadságharc",
+        tan2:"magyar",
     },
     {
-        Időszak:"XIX. század",
-        Évszám:"1812",
-        Esemény:"Napóleon oroszországi hadjárata",
-        Tananyag:"egyetemes",
-        Évszám2:"1809",
-        Esemény2:"győri csata",
-        Tananyag2:"magyar",
+        korszak:"XIX. század",
+        evszam1:"1812",
+        megnev1:"Napóleon oroszországi hadjárata",
+        tan1:"egyetemes",
+        evszam2:"1809",
+        megnev2:"győri csata",
+        tan2:"magyar",
     },
 ]
 
@@ -120,7 +120,7 @@ class T_Table{
         this.#headmaker(headerData);
 
         for (const i of defaultData){
-            this.#addRow(i);
+            this.addRow(i);
         }
     }
     /**
@@ -138,24 +138,24 @@ class T_Table{
     /**
      * @param {TableRow} row 
      */
-    #addRow(row){
+    addRow(row){
         const tr = document.createElement("tr");
         this.#tbody.appendChild(tr);
         
-        const ido = this.#tder(row.Időszak, tr ,"gray");
-        this.#tder(row.Évszám, tr);
-        this.#tder(row.Esemény, tr, "gray");
-        this.#tder(row.Tananyag, tr);
+        const ido = this.#tder(row.korszak, tr ,"gray");
+        this.#tder(row.evszam1, tr);
+        this.#tder(row.megnev1, tr, "gray");
+        this.#tder(row.tan1, tr);
 
-        if (row.Évszám2 && row.Esemény2 && row.Tananyag2){
+        if (row.evszam2 && row.megnev2 && row.tan2){
             ido.rowSpan = 2
 
             const tr = document.createElement("tr");
             this.#tbody.appendChild(tr);
 
-            this.#tder(row.Évszám2, tr);
-            this.#tder(row.Esemény2, tr, "gray");
-            this.#tder(row.Tananyag2, tr);
+            this.#tder(row.evszam2, tr);
+            this.#tder(row.megnev2, tr, "gray");
+            this.#tder(row.tan2, tr);
         }
     }
     /**
@@ -171,8 +171,6 @@ class T_Table{
         return td;
     }
 }
-
-
 
 
 class F_Form{
@@ -206,6 +204,8 @@ class F_Form{
         const button = document.createElement("button");
         this.#form.appendChild(button);
         button.innerHTML = "Hozzáadás";
+
+        this.#form.addEventListener("submit", this.#event);
     }
     /**
      * @param {FormRow} row 
@@ -240,8 +240,69 @@ class F_Form{
             return input;
         }
     }
+    /**
+     * @param {Event} e 
+     */
     #event = (e) => {
-        
+        e.preventDefault()
+        const reqformInput = {
+            korszak:this.#fields.korszak.value,
+            evszam1:this.#fields.evszam1.value,
+            megnev1:this.#fields.megnev1.value,
+            tan1:this.#fields.tan1.value,
+        };
+        const optFormInput = {
+            evszam2:this.#fields.evszam2.value,
+            megnev2:this.#fields.megnev2.value,
+            tan2:this.#fields.tan2.value,
+        }
+        const data = this.#validate(reqformInput, optFormInput);
+        console.log(data)
+        if (data){
+            this.#table.addRow(data);
+        }
+    }
+
+    #validate(reqFormInput, optFormInput){
+        for (const i in this.#errors){
+            this.#errors[i].innerHTML = "";
+        }
+
+        let good = true;
+        for (const i in reqFormInput){
+            if (!reqFormInput[i]){
+                this.#errors[i + "Err"].innerHTML = "Ezt a mezőt is ki kell tölteni.";
+                good = false;
+            }
+        }
+
+        let filledOpt = 0;
+        for (const i in optFormInput){
+            if (optFormInput[i]){
+                filledOpt++;
+            }
+        }
+
+        if (filledOpt != 3 && filledOpt != 0){
+            for (const i in optFormInput){
+                if (!optFormInput[i]){
+                    this.#errors[i.toLowerCase() + "Err"].innerHTML = "Ezt a mezőt is ki kell tölteni.";
+                }
+            }
+            good = false;
+        } else if (filledOpt == 3) {
+            for (const i in optFormInput){
+                reqFormInput[i] = optFormInput[i];
+            }
+        }   
+
+        if (good){
+            for (const i in this.#fields){
+                this.#fields[i].value = "";
+            }
+            return reqFormInput;
+        }
+        return null;
     }
 }
 
